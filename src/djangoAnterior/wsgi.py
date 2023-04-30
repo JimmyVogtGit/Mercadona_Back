9fde10aab959c1be90f2e41b1704fc074ff46ec2
JimmyVogtGit <vogt.jimmy18@gmail.com>
2023-04-30 20:53:46 +0200

    change wsgi.py

--- src/djangoAnterior/wsgi.py
+++ src/djangoAnterior/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/home/jimbigood/Documents/projects/studi/Django/django_with_postgresql_9622/Back/src')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoAnterior.settings')
 
