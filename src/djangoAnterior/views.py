56fd5fe13408613d2278cd29e4b5140789e7a612
JimmyVogtGit <vogt.jimmy18@gmail.com>
2023-04-30 21:29:48 +0200

    change httpresponse value

--- src/djangoAnterior/views.py
+++ src/djangoAnterior/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>Hello, world!</h1>")
+    return HttpResponse("<h1>Salut Alison, je pense fort à toi !!!</h1>")
