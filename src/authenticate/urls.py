056ad0e5e822518a60d49aca11dcd052687f2b3c
JimmyVogtGit <vogt.jimmy18@gmail.com>
2023-05-05 14:46:15 +0200

    add new message for user create + users_list controller

--- src/authenticate/urls.py
+++ src/authenticate/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_user, login_user
+from .views import create_user, login_user, users_list
 
 urlpatterns = [
     path('create-user/', create_user, name='create_user'),
-    path('login-user/', login_user, name='login_user')
+    path('login-user/', login_user, name='login_user'),
+    path('users-list/', users_list, name='users_list')
 ]
--- src/authenticate/views.py
+++ src/authenticate/views.py
@@ -2,6 +2,8 @@ import json
 from django.contrib.auth import get_user_model,authenticate, login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+
 
 
 User = get_user_model()
@@ -18,7 +20,7 @@ def create_user(request):
             return JsonResponse({'error': 'Username already exists'})
         if email and name and password:
             User.objects.create_user(username=name,email=email, password=password)
-            return JsonResponse({'success': True})
+            return JsonResponse({'success': True, 'message' : 'Utilisateur ajouté avec succès !'})
         else:
             return JsonResponse({'success': False, 'message': 'Veuillez remplir tous les champs.'})
     else:
@@ -34,5 +36,15 @@ def login_user(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return JsonResponse({'success': True})
+            return JsonResponse({'success': True, 'userName': username})
     return JsonResponse({'success': False})
+
+def users_list(request):
+    users = User.objects.all()
+    usersList = []
+    for user in users:
+        usersList.append({
+            'name': user.username,
+            'email': user.email
+        })
+    return JsonResponse(usersList, safe=False)
