518bd59b8535ceb61555f2dad5976e68c8e70e59
JimmyVogtGit <vogt.jimmy18@gmail.com>
2023-05-08 21:22:01 +0200

    new migrations + create_promo + upgrade product_list with promo

--- src/product/models.py
+++ src/product/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 
 def upload_to(instance, filename):
     return '/'.join(['media', str(instance.wording), filename])
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -11,12 +13,6 @@ class Category(models.Model):
     def __str__(self):
         return self.nom
 
-class Promotion(models.Model):
-    name = models.CharField(max_length=255)
-    percentage = models.IntegerField()
-
-    def __str__(self):
-        return self.nom
 
 class Produit(models.Model):
     wording = models.CharField(max_length=255)
@@ -24,7 +20,6 @@ class Produit(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to=upload_to, blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    promotion = models.ForeignKey(Promotion, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.wording
@@ -36,3 +31,13 @@ class Produit(models.Model):
             return reduce_price
         else:
             return self.price
+
+
+class Promotion(models.Model):
+    percentage = models.IntegerField(null=False, blank=False)
+    startDate = models.DateTimeField(null=True, blank=True)
+    endDate = models.DateTimeField(null=True, blank=True)
+    produit = models.ForeignKey(Produit, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.percentage
--- src/product/views.py
+++ src/product/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
-
 # Create your views here.
 import json
+from datetime import datetime
+
+import pytz
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
 from .models import Category, Produit, Promotion
 
-
+tz = pytz.timezone('Europe/Paris')
 
 """class ProduitViewSet(viewsets.ModelVieSet):
     queryset = Produit.objects.all()
@@ -41,12 +43,10 @@ from .models import Category, Produit, Promotion
             return JsonResponse({'status': 'error', 'message': 'Invalid request method'})"""
 
 
-
 @csrf_exempt
 def create_product(request):
     if request.method == 'POST':
         data = json.loads(request.body.decode("utf-8"))
-        print(data)
         wording = data.get('wording')
         describe = data.get('describe')
         price = data.get('price')
@@ -54,7 +54,6 @@ def create_product(request):
         category_id = data.get('category')
         promotion_id = data.get('promotion')
         category = Category.objects.get(pk=category_id)
-        promotion = Promotion.objects.get(pk=promotion_id) if promotion_id else None
 
         produit = Produit(
             wording=wording,
@@ -62,7 +61,7 @@ def create_product(request):
             price=price,
             # image=image,
             category=category,
-            promotion=promotion
+
         )
 
         produit.save()
@@ -71,17 +70,27 @@ def create_product(request):
     else:
         return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
 
+
 def product_list(request):
-    products = Produit.objects.select_related('category', 'promotion')
+    products = Produit.objects.prefetch_related('promotion_set', 'category')
     products_list = []
+
     for product in products:
+        promotions = []
+        for promotion in product.promotion_set.all():
+            promotions.append({
+                'id': promotion.id,
+                'percentage': promotion.percentage
+            })
         products_list.append({
-            'id':product.id,
-            'wording':product.wording,
-            "describe":product.describe,
+            'id': product.id,
+            'wording': product.wording,
+            "describe": product.describe,
             "price": product.price,
-            "category":product.category.name,
+            "category": product.category.name,
             "image": product.image if product.image else None,
+            "promotion": promotions
+
         })
     return JsonResponse(products_list, safe=False)
 
@@ -91,7 +100,6 @@ def create_category(request):
     if request.method == 'POST':
         data = json.loads(request.body.decode("utf-8"))
         wording = data.get('wording')
-
         category = Category(wording=wording)
         category.save()
 
@@ -99,12 +107,38 @@ def create_category(request):
     else:
         return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
 
+
 def category_list(request):
     categories = Category.objects.all()
     category_list = []
     for category in categories:
         category_list.append({
-            'id':category.id,
-            'name':category.name
+            'id': category.id,
+            'name': category.name
         })
     return JsonResponse(category_list, safe=False)
+
+
+@csrf_exempt
+def create_promotion(request):
+    if request.method == "POST":
+        data = json.loads(request.body.decode("utf-8"))
+        percent = data.get("percent")
+        start_date = tz.localize(datetime.strptime(data.get("startDate"), '%Y-%m-%d'))
+        end_date = tz.localize(datetime.strptime(data.get("endDate"), '%Y-%m-%d'))
+        product_id = data.get("productId")
+
+    existing_product = Produit.objects.get(id=product_id)
+    existing_promotion = Promotion.objects.filter(produit=product_id).first()
+
+    if existing_promotion is not None:
+        existing_promotion.percentage = percent
+        existing_promotion.startDate = start_date
+        existing_promotion.endDate = end_date
+        existing_promotion.save()
+        return JsonResponse({"status": "success", "message": "Modified promotion with success"})
+
+    else:
+        promotion = Promotion(percentage=percent, startDate=start_date, endDate=end_date, produit=existing_product)
+        promotion.save()
+        return JsonResponse({"status": "success", "message": "Creating promotion with success"})
