3a147fa14383fc56be1f7d83a7d5d06069329e14
JimmyVogtGit <vogt.jimmy18@gmail.com>
2023-05-17 00:36:17 +0200

    change path

--- src/djangoAnterior/urls.py
+++ src/djangoAnterior/urls.py
@@ -6,14 +6,10 @@ from django.urls import re_path
 from . import views
 import os
 
-current_dir = os.path.abspath(os.path.dirname(__file__))
-public_html_dir = os.path.abspath(os.path.join(current_dir, '../../../../public_html'))
-
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
     path('', include('authenticate.urls')),
     path('', include('product.urls')),
-    path('', views.index),
-    re_path(r'^.*$', serve, {'document_root': public_html_dir})
 
 ]
--- src/djangoAnterior/views.py
+++ src/djangoAnterior/views.py
@@ -1,15 +1,5 @@
-import os
-
 from django.http import HttpResponse
 
 
 def index(request):
-    current_dir = os.path.abspath(os.path.dirname(__file__))
-    public_html_dir = os.path.abspath(os.path.join(current_dir, '../../../../public_html'))
-    index_html = os.path.join(public_html_dir, 'index.html')
-
-    # Lire le contenu du fichier index.html
-    with open(index_html, 'r') as file:
-        content = file.read()
-
-    return HttpResponse(content)
+    return HttpResponse("<h1>Helo, world !</h1>")
