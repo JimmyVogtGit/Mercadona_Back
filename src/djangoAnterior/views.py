269180c10693836c3b08a8ffaee5c37dd61e7f48
JimmyVogtGit <vogt.jimmy18@gmail.com>
2023-05-17 00:31:09 +0200

    change path

--- src/djangoAnterior/views.py
+++ src/djangoAnterior/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    public_html_dir = os.path.abspath(os.path.join(current_dir, '../../../../../public_html'))
+    public_html_dir = os.path.abspath(os.path.join(current_dir, '../../../../public_html'))
     index_html = os.path.join(public_html_dir, 'index.html')
 
     # Lire le contenu du fichier index.html
